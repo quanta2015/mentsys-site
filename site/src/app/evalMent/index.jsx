c68829bba29aafd87d4674ba7b0fb29509da6e75
missht0 <missht0@163.com>
2023-03-14 01:30:01 +0800

    取消tab页，添加默认，显示总分

--- site/src/app/evalMent/index.jsx
+++ site/src/app/evalMent/index.jsx
@@ -16,7 +16,7 @@ import { EDU_STU_LIST } from "@/constant/urls";
 
 const EvalMent = () => {
   const [form] = Form.useForm();
-  const [grade, setGrade] = React.useState(Array(5).fill(0));
+  const [grade, setGrade] = React.useState("不合格");
   const [gradearr, setGradearr] = React.useState(() => {
     let arr = [];
     EDU_STU_LIST.forEach((item) => {
@@ -25,15 +25,12 @@ const EvalMent = () => {
     return arr;
   });
 
-  const getsum = (val, index) => {
+  const getsum = (val, index0, index) => {
     setGradearr((gradearr) => {
-      gradearr[activeKey][index] = val;
-      setGrade((grade) => {
-        let newgrade = [...grade];
-        newgrade[activeKey] = eval(
-          gradearr[activeKey].toString().split(",").join("+")
-        );
-        return newgrade;
+      gradearr[index0][index] = val;
+      setGrade(() => {
+        let newgrade = eval(gradearr.toString().split(",").join("+"));
+        return newgrade > 30 ? "优秀" : newgrade > 10 ? "合格" : "不合格";
       });
       return gradearr;
     });
@@ -46,103 +43,29 @@ const EvalMent = () => {
     return value;
   };
 
-  const [activeKey, setActiveKey] = React.useState(0);
-  const { name, id, uid } = useParams();
-  // 思想育人 科研育人 创新创业育人 实践育人 其他课外育人
-  const renderContent = (item) => {
-    return (
-      <div className={s.mform}>
-        <div className={s.tit}>
-          {`信息科学与技术学院${item.label}考核表`}
-          <div className={s.sp}>{`总分：${grade[activeKey]}`}</div>
-        </div>
-
-        <div>
-          {EDU_STU_LIST[activeKey]?.map((item, index) => {
-            return (
-              <>
-                <Form.Item
-                  label={item.name}
-                  rules={[{ required: true, message: "必填" }]}
-                  key={item.index}
-                  name={`${activeKey}_${index}`}
-                >
-                  {item.list.length > 0 ? (
-                    <Radio.Group
-                      onChange={(e) => {
-                        getsum(e.target.value, index);
-                      }}
-                    >
-                      {item.list.map((i, index) => {
-                        return (
-                          <Radio.Button value={i.v} key={index}>
-                            {i.m}
-                          </Radio.Button>
-                        );
-                      })}
-                    </Radio.Group>
-                  ) : (
-                    item.input && (
-                      <InputNumber
-                        min={0}
-                        max={item.input.m ?? undefined}
-                        step={0.5}
-                        formatter={checkhalf}
-                        style={{ width: "100%" }}
-                        placeholder={`请输入${item.input.i}`}
-                        key={index}
-                        onChange={(e) => {
-                          getsum(e * item.input.v, index);
-                        }}
-                      />
-                    )
-                  )}
-                </Form.Item>
-              </>
-            );
-          })}
-        </div>
-        <div className={s.mft}>
-          <Button
-            type="primary"
-            onClick={() => {
-              console.log(form.getFieldsValue());
-              console.log(grade, gradearr);
-            }}
-          >
-            保存
-          </Button>
-        </div>
-      </div>
-    );
+  const initialvaluesform = async () => {
+    let obj = {};
+    EDU_STU_LIST.map((item0, index0) => {
+      item0.map((item, index) => {
+        obj[`${index0}_${index}`] = 0;
+      });
+    });
+    console.log("obj", obj);
+    return obj;
   };
 
+  useEffect(() => {
+    initialvaluesform().then((res) => {
+      form.setFieldsValue(res);
+    });
+  }, []);
+
   const items = [
-    {
-      key: 0,
-      label: `思想育人`,
-      children: renderContent({ label: `思想育人`, key: "1" }),
-    },
-    {
-      key: 1,
-      label: `科研育人`,
-      children: renderContent({ label: `科研育人`, key: "2" }),
-    },
-    {
-      key: 2,
-      label: `创新创业育人`,
-      children: renderContent({ label: `创新创业育人`, key: "3" }),
-    },
-    {
-      key: 3,
-      label: `实践育人`,
-      children: renderContent({ label: `实践育人`, key: "4" }),
-    },
-    {
-      key: 4,
-      label: `其他课外育人`,
-      children: renderContent({ label: `其他课外育人`, key: "5" }),
-    },
+    `思想育人`,
+    `科研育人`,
+    `创新创业育人`,
+    `实践育人`,
+    `其他课外育人`,
   ];
   return (
     <div className={s.main}>
@@ -153,15 +76,108 @@ const EvalMent = () => {
           labelCol: { span: 16 },
           wrapperCol: { span: 8 },
         }}
+        labelCol={{
+          style: {
+            width: "66%",
+            whiteSpace: "normal",
+            marginRight: "50px",
+          },
+        }}
         labelAlign="left"
       >
-        <Tabs
-          defaultActiveKey="1"
-          items={items}
-          onChange={(key) => {
-            setActiveKey(key);
-          }}
-        />
+        <div className={s.mform}>
+          <div className={s.tit}>
+            {`信息科学与技术学院课外育人考核表`}
+            {<div className={s.sp}>{`评价：${grade}`}</div>}
+          </div>
+        </div>
+
+        {EDU_STU_LIST?.map((item0, index0) => {
+          return (
+            <div className={s.mform}>
+              <div className={s.tit2}>{`${items[index0]}`}</div>
+
+              <div>
+                {item0?.map((item, index) => {
+                  return (
+                    <>
+                      <Form.Item
+                        extra={item.input?.i ? `请输入${item.input.i}` : null}
+                        label={
+                          <>
+                            <span
+                              style={{
+                                marginRight: "25px",
+                                color: "var(--clr-main)",
+                                width: "50px",
+                              }}
+                            >
+                              {gradearr[index0][index]}
+                              <span
+                                style={{
+                                  marginLeft: "5px",
+                                }}
+                              >
+                                分
+                              </span>
+                            </span>
+                            {item.name}
+                          </>
+                        }
+                        rules={[{ required: true, message: "必填" }]}
+                        key={item.index}
+                        name={`${index0}_${index}`}
+                      >
+                        {item.list.length > 0 ? (
+                          <Radio.Group
+                            onChange={(e) => {
+                              getsum(e.target.value, index0, index);
+                            }}
+                          >
+                            {item.list.map((i, index) => {
+                              return (
+                                <Radio.Button value={i.v} key={index}>
+                                  {i.m}
+                                </Radio.Button>
+                              );
+                            })}
+                          </Radio.Group>
+                        ) : (
+                          item.input && (
+                            <InputNumber
+                              min={0}
+                              max={item.input.m ?? undefined}
+                              formatter={checkhalf}
+                              style={{ width: "100%" }}
+                              placeholder={`请输入${item.input.i}`}
+                              key={index}
+                              onChange={(e) => {
+                                getsum(e * item.input.v, index0, index);
+                              }}
+                            />
+                          )
+                        )}
+                      </Form.Item>
+                    </>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+        <div className={s.mform}>
+          <div className={s.mft}>
+            <Button
+              type="primary"
+              onClick={() => {
+                console.log(form.getFieldsValue());
+                console.log(grade, gradearr);
+              }}
+            >
+              保存
+            </Button>
+          </div>
+        </div>
       </Form>
     </div>
   );
